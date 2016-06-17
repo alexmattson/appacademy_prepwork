f8f2c80c55a69c5af413d11f7e89d7b4d19ee288
Alex Mattson <amattson21@gmail.com>
2016-06-17 17:25:34 +0000

    Completed w3d1 01_xml_document

--- w3/w3d1/lib/01_xml_document.rb
+++ w3/w3d1/lib/01_xml_document.rb
@@ -1,3 +1,37 @@
 class XmlDocument
-  # TODO: your code goes here!
+  
+  def initialize(indent = false)
+    @XmlDocument = ""
+    @indent = indent
+    @indent_num = 0
+  end
+  
+  def method_missing(method_name, *params, &blk)
+    if block_given?
+      indent('up') if @indent 
+      @XmlDocument << "<#{method_name}>"
+      @XmlDocument << "\n" if @indent
+      @XmlDocument << yield
+      indent('down') if @indent
+      @XmlDocument << "</#{method_name}>"
+      @XmlDocument << "\n" if @indent
+    else
+      indent('down') if @indent
+      @XmlDocument += "<#{method_name.to_s}#{arguments(params)}/>"
+      @XmlDocument += "\n" if @indent
+    end
+    @XmlDocument
+  end
+
+  def indent(arg)
+    @indent_num.times{ @XmlDocument << " " }
+    arg == "up" ? @indent_num += 2 : @indent_num -= 2 
+  end 
+  
+  def arguments(params)
+    final =""
+    args = params[0] || {}
+    args.each { |key, value| final += " #{key}=\"#{value}\"" }
+    final
+  end
 end
